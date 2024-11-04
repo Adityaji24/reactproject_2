67912e6ee17c0a60c17454d43fd4c4f76999cfb6
adityaji24 <adityatanwani059@gmail.com>
2024-11-04 13:26:01 +0530

    New changes made

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,14 +9,13 @@ import concertImg from "./images/concert.jpg";
 import artImg from "./images/art.jpg";
 import foodImg from "./images/food2.jpeg";
 import techImg from "./images/tech.jpg";
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 const Home = () => {
   const events = [
-    { title: "Music Concert", date: "Oct 30, 2024 ",image: concertImg, location: "Central Park,Delhi", description: "Join us for an evening of great music featuring local artists." },
-    { title: "Art Exhibition", date: "Nov 10, 2024", image: artImg, location: "Art Gallery,Indore", description: "Explore stunning artworks by emerging artists in our annual exhibition." },
-    { title: "Food Festival", date: "Nov 15, 2024", image: foodImg, location: "City Square,Noida", description: "Taste dishes from around the world at our exciting food festival." },
-    { title: "Tech Summit", date: "Dec 5, 2024", image: techImg, location: "Tech Hub,Bhopal", description: "A gathering of innovators and tech enthusiasts discussing the future of technology." },
+    { title: "Music Concert", date: "Oct 30, 2024 ", image: concertImg, location: "Central Park, Delhi", description: "Join us for an evening of great music featuring local artists." },
+    { title: "Art Exhibition", date: "Nov 10, 2024", image: artImg, location: "Art Gallery, Indore", description: "Explore stunning artworks by emerging artists in our annual exhibition." },
+    { title: "Food Festival", date: "Nov 15, 2024", image: foodImg, location: "City Square, Noida", description: "Taste dishes from around the world at our exciting food festival." },
+    { title: "Tech Summit", date: "Dec 5, 2024", image: techImg, location: "Tech Hub, Bhopal", description: "A gathering of innovators and tech enthusiasts discussing the future of technology." },
   ];
 
   return (
@@ -33,8 +32,6 @@ const Home = () => {
         ))}
       </div>
 
-      {/* Testimonials Section */}
-      
       {/* Call-to-Action Section */}
       <div className="cta-section">
         <h2>Stay Updated!</h2>
@@ -47,7 +44,6 @@ const Home = () => {
         <p>&copy; 2024 Event Hub. All rights reserved.</p>
         <p>Follow us on social media for updates!</p>
         <div className="social-media-icons">
-          {/* Add icons for social media (you can use Font Awesome or similar libraries) */}
           <i className="fa-brands fa-instagram"></i>
           <i className="fa-brands fa-google"></i>
           <i className="fa-brands fa-linkedin"></i>
